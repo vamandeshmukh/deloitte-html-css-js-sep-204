7c085baaa3d38a7d189f12a8236f3f5d2b55c427
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-23 15:40:56 +0530

    html

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+// console.log('Hello world!');
+
+
+// variable declaration in JS 
+
+// java 
+// int num = 10;
+
+// javascript 
+
+// num = 10; // do not use this 
+// var num2 = 20; // do not use this 
+// const num3 = 30; // preferred choice for constants  
+// let num4 = 40; // use for variables 
+
+// const num = 10;
+// console.log(num);
+// num = 20;
+// console.log(num);
+
+// dynamically typed language 
+
+let myVar;
+console.log(myVar);
+console.log(typeof (myVar));
+myVar = 10;
+console.log(myVar);
+console.log(typeof (myVar));
+myVar = 20;
+console.log(myVar);
+console.log(typeof (myVar));
+myVar = "abc";
+console.log(myVar);
+console.log(typeof (myVar));
+myVar = false;
+console.log(myVar);
+console.log(typeof (myVar));
+
