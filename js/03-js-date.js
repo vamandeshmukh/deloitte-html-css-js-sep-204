f5833b0acbd7838e69bb07b4dd305b4fa44d6301
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-24 12:37:01 +0530

    callback in js

---
+++ js/03-js-date.js
@@ -0,0 +1,11 @@
+
+const now = new Date();
+console.log(now);
+
+console.log(now.getDate(), now.getMonth(), now.getFullYear());
+
+const myDate = new Date(2024, 9, 24);
+console.log(myDate);
+
+console.log(Math.pow(2, 3));
+
---
+++ js/04-js-dom.js
@@ -0,0 +1,5 @@
+console.log('dom');
+
+const elem = document.getElementById('para');
+elem.textContent = 'Hello world!';
+elem.style.color = 'blue';
