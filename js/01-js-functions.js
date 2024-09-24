15124dc0d802f20f1e2513e56386d2f281d1d708
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-24 09:57:47 +0530

    callback in js

--- js/01-js-functions.js
+++ js/01-js-functions.js
@@ -31,3 +31,13 @@ fun(() => { console.log('anonymous function invoked.'); });
 // fun(callbackFun);
 
 
+
+// // FUNCTION value 
+const fun2 = (arg) => {
+    arg({ city: 'Pune' }); // function call 
+    console.log('fun function invoked');
+};
+
+fun2(() => {
+    console.log('anonymous function invoked.');
+});
